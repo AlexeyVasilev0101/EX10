ca00bd8d5c889144ac0c5574086e1e3552262305
AlexeyVasilev0101 <101921598+AlexeyVasilev0101@users.noreply.github.com>
2022-05-06 16:17:14 +0300

    EX10

---
+++ EX10OOP/EX10OOP.cpp
@@ -0,0 +1,10 @@
+﻿#include "human.h"
+
+int main()
+{
+    Student Alexey("Vasilev", "Alexey", "Valeryevich", 20, true);
+    Boss Pirogova("Pirogova", "Tatyana", "Grigoryevna", 58, 135);
+    Alexey.print();
+    cout << endl;
+    Pirogova.print();
+}
